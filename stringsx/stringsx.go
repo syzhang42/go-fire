8557d72245e7b7cf6b9c421c5d03abf3854dcda4
syzhang42 <syzhang42@iflytek.com>
2024-08-23 14:15:34 +0800

    splict -> split

--- stringsx/stringsx.go
+++ stringsx/stringsx.go
@@ -2,7 +2,7 @@ package stringsx
 
 import "strings"
 
-func Splict(str1 string, str2 string) []string {
+func Split(str1 string, str2 string) []string {
 	strs := strings.Split(str1, str2)
 	if len(strs) == 0 || len(strs) == 1 && strs[0] == "" {
 		return []string{}
