5a165b97d1a93a23790ac85724f0f80afa4e3842
syzhang42 <syzhang42@iflytek.com>
2024-09-13 17:44:25 +0800

    add configor

---
+++ configor/configor.go
@@ -0,0 +1,52 @@
+// 搬运自github.com/cocktail828/go-tools
+package configor
+
+import (
+	"github.com/BurntSushi/toml"
+	"github.com/go-playground/validator/v10"
+)
+
+type Configor struct {
+	LoadEnv   bool            // 是否读取环境变量
+	EnvPrefix string          // 环境变量前缀
+	Unmarshal Unmarshal       // 解析器
+	Validator func(any) error // 校验器
+}
+
+var Default = &Configor{
+	LoadEnv:   false,
+	EnvPrefix: "",
+	Unmarshal: toml.Unmarshal,
+	Validator: validator.New().Struct,
+}
+
+func (c *Configor) Load(dst any, payload ...[]byte) (err error) {
+	if err := c.load(dst, payload...); err != nil {
+		return err
+	}
+	if c.Validator != nil {
+		return c.Validator(dst)
+	}
+	return nil
+}
+
+// Load will unmarshal configurations to struct from files that you provide
+func (c *Configor) LoadFile(dst any, files ...string) error {
+	if err := c.loadFile(dst, files...); err != nil {
+		return err
+	}
+	if c.Validator != nil {
+		return c.Validator(dst)
+	}
+	return nil
+}
+
+// Load will unmarshal configurations to struct from files that you provide
+func Load(dst any, payload ...[]byte) error {
+	return Default.Load(dst, payload...)
+}
+
+// Load will unmarshal configurations to struct from files that you provide
+func LoadFile(dst any, files ...string) error {
+	return Default.LoadFile(dst, files...)
+}
--- go.mod
+++ go.mod
@@ -3,21 +3,30 @@ module github.com/syzhang42/go-fire
 go 1.20
 
 require (
+	github.com/BurntSushi/toml v0.3.1
 	github.com/apache/pulsar-client-go v0.13.0
+	github.com/go-playground/validator/v10 v10.22.1
 	github.com/lestrrat/go-file-rotatelogs v0.0.0-20180223000712-d3151e2a480f
 	github.com/sirupsen/logrus v1.9.3
 	golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa
 	golang.org/x/sys v0.20.0
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
 	github.com/jonboulle/clockwork v0.4.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/lestrrat/go-envload v0.0.0-20180220120943-6ed08b54a570 // indirect
 	github.com/lestrrat/go-strftime v0.0.0-20180220042222-ba3bf9c1d042 // indirect
 	github.com/tebeka/strftime v0.1.5 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
 
 require (
