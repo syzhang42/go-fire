2baac493ca5405ac2280e5fd8b131ebf6ae63b14
syzhang42 <syzhang42@iflytek.com>
2025-06-27 17:44:48 +0800

    add slice

--- STL/slice/slice.go
+++ STL/slice/slice.go
@@ -8,8 +8,16 @@ func Slice2Map[T comparable](in []T) map[T]struct{} {
 	return out
 }
 
-func Map2Slice[T comparable](in map[T]struct{}) []T {
-	out := make([]T, len(in))
+func MapVal2Slice[T1 comparable, T2 any](in map[T1]T2) []T2 {
+	out := make([]T2, len(in))
+	for _, v := range in {
+		out = append(out, v)
+	}
+	return out
+}
+
+func MapKey2Slice[T1 comparable, T2 any](in map[T1]T2) []T1 {
+	out := make([]T1, len(in))
 	for k := range in {
 		out = append(out, k)
 	}
